58c2e2b8a49e7e564766cc6e405d9f47fc101391
Wahyu Maulana <mwahyu7403@gmail.com>
2024-03-19 11:27:39 +0700

    01_Class_New

--- my_oop/bin/01_class.dart
+++ my_oop/bin/01_class.dart
@@ -7,14 +7,3 @@ class Motor{
     print("Tetetetettett!");
   }
 }
-
-void main(){
-  Motor varMotor = Motor();
-  varMotor.merk = "Yamaha";
-  varMotor.model = "Nmax";
-  varMotor.tahun = 2023;
-
-  print(varMotor.merk);
-  print(varMotor.model);
-  print(varMotor.tahun);
-}
